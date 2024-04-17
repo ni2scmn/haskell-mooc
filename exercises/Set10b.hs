cb612380944066a92051a0e89c9aee570d2d7fc2
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2024-04-17 19:13:16 +0200

    finish ex10b

--- exercises/Set10b.hs
+++ exercises/Set10b.hs
@@ -22,7 +22,8 @@ import Mooc.Todo
 --   False ||| undefined ==> an error!
 
 (|||) :: Bool -> Bool -> Bool
-x ||| y = todo
+_ ||| True = True
+x ||| False = x
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the function boolLength, that returns the length of a
@@ -36,7 +37,9 @@ x ||| y = todo
 --   length [False,undefined] ==> 2
 
 boolLength :: [Bool] -> Int
-boolLength xs = todo
+boolLength [] = 0
+boolLength (x:xs) = if True ||| x then y else y where
+  y = 1 + boolLength xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Define the function validate which, given a predicate and a
@@ -50,7 +53,7 @@ boolLength xs = todo
 --   validate (\x -> undefined) 3  ==>  an error!
 
 validate :: (a -> Bool) -> a -> a
-validate predicate value = todo
+validate predicate value = if predicate value then value else value
 
 ------------------------------------------------------------------------------
 -- Ex 4: Even though we can't implement the generic seq function
@@ -84,10 +87,13 @@ class MySeq a where
   myseq :: a -> b -> b
 
 instance MySeq Bool where
-  myseq = todo
+  myseq True x = x
+  myseq False x = x
 
 instance MySeq Int where
-  myseq = todo
+  myseq 0 x = x
+  myseq n x = x 
 
 instance MySeq [a] where
-  myseq = todo
+  myseq [] x = x
+  myseq l  x = x
