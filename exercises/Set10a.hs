46de5c130b9dbcd6617f4ef9da6cad4b47b500ce
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2024-04-17 19:05:17 +0200

    finish ex10a

--- exercises/Set10a.hs
+++ exercises/Set10a.hs
@@ -3,6 +3,8 @@ module Set10a where
 import Data.Char
 import Data.List
 
+import Debug.Trace
+
 import Mooc.Todo
 
 ------------------------------------------------------------------------------
@@ -16,7 +18,8 @@ import Mooc.Todo
 --   take 10 (doublify [0..])  ==>  [0,0,1,1,2,2,3,3,4,4]
 
 doublify :: [a] -> [a]
-doublify = todo
+doublify [] = []
+doublify (x:xs) = x:x:doublify xs
 
 ------------------------------------------------------------------------------
 -- Ex 2: Implement the function interleave that takes two lists and
@@ -37,7 +40,17 @@ doublify = todo
 --   take 10 (interleave [1..] (repeat 0)) ==> [1,0,2,0,3,0,4,0,5,0]
 
 interleave :: [a] -> [a] -> [a]
-interleave = todo
+interleave [] [] = []
+interleave a  [] = a
+interleave [] b  = b
+interleave a b = ilLeft a b
+
+ilLeft [] b = b
+ilLeft (a:as) b = a: ilRight as b
+
+ilRight a [] = a
+ilRight a (b:bs) = b: ilLeft a bs
+
 
 ------------------------------------------------------------------------------
 -- Ex 3: Deal out cards. Given a list of players (strings), and a list
@@ -56,7 +69,7 @@ interleave = todo
 -- Hint: remember the functions cycle and zip?
 
 deal :: [String] -> [String] -> [(String,String)]
-deal = todo
+deal a cards = zip cards (cycle a)
 
 ------------------------------------------------------------------------------
 -- Ex 4: Compute a running average. Go through a list of Doubles and
@@ -74,7 +87,10 @@ deal = todo
 
 
 averages :: [Double] -> [Double]
-averages = todo
+averages [] = []
+averages (x:xs) = avg' xs x 1 where
+  avg' [] prev_sum prev_count = [prev_sum / prev_count]
+  avg' (x:xs) prev_sum prev_count = (prev_sum / prev_count):avg' xs (prev_sum + x) (prev_count + 1)
 
 ------------------------------------------------------------------------------
 -- Ex 5: Given two lists, xs and ys, and an element z, generate an
@@ -92,7 +108,7 @@ averages = todo
 --   take 10 (alternate [1,2] [3,4,5] 0) ==> [1,2,0,3,4,5,0,1,2,0]
 
 alternate :: [a] -> [a] -> a -> [a]
-alternate xs ys z = todo
+alternate xs ys z = xs ++ [z] ++ ys ++ [z] ++ (alternate xs ys z)
 
 ------------------------------------------------------------------------------
 -- Ex 6: Check if the length of a list is at least n. Make sure your
@@ -104,7 +120,9 @@ alternate xs ys z = todo
 --   lengthAtLeast 10 [0..]  ==> True
 
 lengthAtLeast :: Int -> [a] -> Bool
-lengthAtLeast = todo
+lengthAtLeast 0 _ = True
+lengthAtLeast n [] = False
+lengthAtLeast n (x:xs) = lengthAtLeast (n-1) xs
 
 ------------------------------------------------------------------------------
 -- Ex 7: The function chunks should take in a list, and a number n,
@@ -122,7 +140,8 @@ lengthAtLeast = todo
 --   take 4 (chunks 3 [0..]) ==> [[0,1,2],[1,2,3],[2,3,4],[3,4,5]]
 
 chunks :: Int -> [a] -> [[a]]
-chunks = todo
+chunks _ [] = []
+chunks n x@(_:xs) = if lengthAtLeast n x then take n x:chunks n xs else []
 
 ------------------------------------------------------------------------------
 -- Ex 8: Define a newtype called IgnoreCase, that wraps a value of
@@ -138,8 +157,14 @@ chunks = todo
 --   ignorecase "abC" == ignorecase "ABc"  ==>  True
 --   ignorecase "acC" == ignorecase "ABc"  ==>  False
 
-ignorecase = todo
+newtype IgnoreCase = IgnoreCase String
 
+ignorecase :: String -> IgnoreCase
+ignorecase s = IgnoreCase s
+
+instance Eq IgnoreCase where
+  (==) (IgnoreCase a) (IgnoreCase b) = map Data.Char.toLower a == map Data.Char.toLower b
+   
 ------------------------------------------------------------------------------
 -- Ex 9: Here's the Room type and some helper functions from the
 -- course material. Define a cyclic Room structure like this:
@@ -182,4 +207,7 @@ play room (d:ds) = case move room d of Nothing -> [describe room]
                                        Just r -> describe room : play r ds
 
 maze :: Room
-maze = todo
+maze = maze1 where
+  maze1 =  Room "Maze" [("Left", maze2), ("Right", maze3)]
+  maze2 = Room "Deeper in the maze" [("Left", maze3), ("Right", maze1)]
+  maze3 = Room "Elsewhere in the maze" [("Left", maze1), ("Right", maze2)]
